7573a3e427451aa30b836559d8ed16f97c68f413
meta-434 <ach1@vt.edu>
2019-05-19 01:55:12 -0400

    added search functionality

--- App.js
+++ App.js
@@ -8,46 +8,65 @@ const posterUrlMaker = imgId => {
   return "http://image.tmdb.org/t/p/w154".concat(imgId)
 }
 
-const queryUrlMaker = title => {
-
-}
-
 const mapResults = result => {
   return result.map(row => 
   <div>
-    <h3>{row.title}</h3>
-    <img src={posterUrlMaker(row.poster_path)} alt={row.title} />
-    <p>Overview: {row.overview} </p>
-    <p>Release Date: {row.release_date} ,
-    ID: {row.id} </p>
+    <button onClick={() => console.log("selection works!")}>
+      <h3>{row.title}</h3>
+      <img src={posterUrlMaker(row.poster_path)} alt={row.title} />
+      <p>Popularity: {row.popularity} </p>
+      <p>Release Date: {row.release_date}</p> 
+      <p>ID: {row.id} </p>
+    </button>
   </div>);
 };
 
 class App extends Component {
-  state = {
-    result: null
-  };
-  componentDidMount() {
-    axios.get("https://api.themoviedb.org/3/search/movie?api_key="+TMDB_API_KEY+"&query=Ocean's").then(response => {
+  
+  constructor(props) {
+    super(props);
+    this.state = {result: null, queryTitle: null};
+
+    this._handleKeyDown = this._handleKeyDown.bind(this);
+  }
+
+
+  _handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      this.setState({queryTitle: event.target.value});
+      console.log(event.target.value)
+      this.apiCallTitle(event);
+    }
+  }
+
+  apiCallID(event) {
+    axios.get().then(response => {
+      console.log(response.data.results);
+      const data = response.data.resutls;
+      this.setState({result : data})
+    });
+  }
+
+  apiCallTitle(event) {
+    axios.get("https://api.themoviedb.org/3/search/movie?api_key=" + TMDB_API_KEY + "&query=" + event.target.value).then(response => {
       console.log(response.data.results);
       const data = response.data.results;
       this.setState({ result: data });
     });
   }
-  
+
   render() {
     return (
       <div className="App">
       <span>
         <h1>
           is 
-          <input type="text" name="searchText" />
+          <span>
+            <input type="text" onKeyDown={this._handleKeyDown} />       
+          </span>
           a good movie?
         </h1>
       </span>
-      <form>
-        <input type="submit" value="Submit" />
-      </form>
       {this.state.result !== null && mapResults(this.state.result)}
       </div>
     );
