8df5d5f0e4b8cae6b7fd2ce8efba12f92db9f73c
meta-434 <ach1@vt.edu>
2019-05-19 22:30:05 -0400

    implement OMDB review functionality

--- App.js
+++ App.js
@@ -9,47 +9,26 @@ const posterUrlMaker = imgId => {
   return "http://image.tmdb.org/t/p/w154".concat(imgId)
 }
 
-const _handleClick = (event) => {
-  apiCallSingular(event);
-}
-
-const apiCallSingular = event => {
-  axios.get("http://www.omdbapi.com/?t=" + event.target.value + "&apikey=" + OMDB_API_KEY).then(response => {
-    console.log(response.data);
-    const data = response.data;
-    this.setState({results : this.data})
-  });
-}
-
-const mapResults = result => {
-  return result.map(row => 
-  <div>
-    <button onClick={_handleClick}>
-      <h3>{row.title}</h3>
-      <img src={posterUrlMaker(row.poster_path)} alt={row.title} />
-      <p>Title : {row.title}</p>
-      <p>Popularity: {row.popularity} </p>
-      <p>Release Date: {row.release_date}</p> 
-      <p>ID: {row.id} </p>
-    </button>
-  </div>);
-};
-
-
 
 class App extends Component {
   
   constructor(props) {
     super(props);
-    this.state = {result: null, queryTitle: null, ratingsInfo: null};
+    this.state = {result: null, queryTitle: null, realName: null, posterUrl: null, ratingInfo: null};
 
     this._handleKeyDown = this._handleKeyDown.bind(this);
   }
-  
+
+  _handleClick = (event) => {
+    event.persist()
+    this.apiCallSingular(event);
+  }
+
   _handleKeyDown = (event) => {
     if (event.key === 'Enter') {
-      this.apiCallTitle(event);
       this.setState({queryTitle: event.target.value});
+      this.apiCallTitle(event);
+      
     }
   }
 
@@ -61,19 +40,57 @@ class App extends Component {
     });
   }
 
+  apiCallSingular = event => {
+    axios.get("http://www.omdbapi.com/?t=" + event.target.alt + "&apikey=" + OMDB_API_KEY).then(response => {
+      console.log(response.data.Ratings)
+      this.setState({result : response.data, realName : event.target.alt, posterUrl : event.target.currentSrc, ratingInfo: response.data.Ratings})
+    });
+  }
+
+  mapResults = result => {
+    if (0 in this.state.result) {
+      return result.map(row => 
+      <div>
+        <button onClick={this._handleClick}>
+          <h3>{row.title}</h3>
+          <img src={posterUrlMaker(row.poster_path)} alt={row.title} />
+          <p>Title : {row.title}</p>
+          <p>Popularity: {row.popularity} </p>
+          <p>Release Date: {row.release_date}</p> 
+          <p>ID: {row.id} </p>
+        </button>
+      </div>);
+    } else {
+      return (
+        <div>
+          <h3>{this.state.realName}</h3>
+          <img src={this.state.posterUrl} alt={this.state.realName} />
+          <div>
+            Ratings:
+            {this.state.ratingInfo.map(item => 
+              <div>
+                Source: {item.Source}, rating: {item.Value}
+              </div>
+              )}
+          </div>
+        </div>
+      );
+    }
+  };
+
   render() {
     return (
       <div className="App">
       <span>
         <h1>
-          is 
+          is  
           <span>
             <input type="text" onKeyDown={this._handleKeyDown} />       
           </span>
-          a good movie?
+            a good movie?
         </h1>
       </span>
-      {this.state.result !== null && mapResults(this.state.result)}
+      {this.state.result !== null && this.mapResults(this.state.result)}
       </div>
     );
   }
